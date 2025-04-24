676a9034dc4e207dce01d6bb095fc13a2a7c274d
Sadriddin <sadriddinravshanov19@gmail.com>
2025-04-24 20:21:54 +0500

    sd

--- components/stats/order-stats-card.tsx
+++ components/stats/order-stats-card.tsx
@@ -1,5 +1,5 @@
-import { useDateFilterStore } from "@/lib/context/date-store";
-import { format } from "date-fns";
+// import { useDateFilterStore } from "@/lib/context/date-store";
+// import { format } from "date-fns";
 import Image, { StaticImageData } from "next/image";
 
 type Props = {
@@ -11,13 +11,13 @@ type Props = {
 };
 
 export default function OrderStatsCard({ icon, title, count, bot1, bot2 }: Props) {
-  const {start_date, end_date} = useDateFilterStore();
+  // const {start_date, end_date} = useDateFilterStore();
   return (
     <div className="bg-white p-3 rounded-2xl w-full flex flex-col justify-between">
       <div className="flex justify-between items-start">
         <div>
           <p className="text-sm text-gray-700 max-w-[170px]">{title}</p>
-          <span className="text-xs text-gray-500">{format(new Date(start_date), "dd - MMMM")} {(start_date || end_date) && "/"} {format(new Date(end_date), "dd - MMMM")}</span>
+          {/* <span className="text-xs text-gray-500">{ start_date && format(new Date(start_date), "dd - MMMM")} {(start_date || end_date) && "/"} { end_date && format(new Date(end_date), "dd - MMMM")}</span> */}
         </div>
           <Image  src={icon} alt={title} className=""/>
       </div>
